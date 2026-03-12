0f7e31384b851c3ff0b39f9f75fb4c8b0a96c5d4
XbiakkoX <raul.kugler@gmail.com>
2026-03-12 19:58:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import ProjectSideBar from "./components/ProjectSideBar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
+
+function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onProjectSelected={handleStartAddProject} />;
+  }
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSideBar onStartAddProject={handleStartAddProject} />
+      {content}
+    </main>
+  );
+}
+
+export default App;
